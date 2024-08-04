ad9358f09453895e00bf4cbacfafa575259ef607
shivanshp2004 <shivanshpathak9988@gmail.com>
2024-08-04 23:33:03 +0530

    deployment bugs fixed

--- src/Components/Restart.jsx
+++ src/Components/Restart.jsx
@@ -27,7 +27,7 @@ const Restart = () => {
 }
    }
    >
-    <img src="../assets/home.png" className='w-[65px] h-[65px]'/>
+    <img src="../../src/assets/home.png" className='w-[65px] h-[65px]'/>
    </button>
     </NavLink>
     </>
