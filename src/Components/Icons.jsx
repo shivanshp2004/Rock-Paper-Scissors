edf7db3e683f9a739293f741d7bd38859107828e
shivanshp2004 <118187132+shivanshp2004@users.noreply.github.com>
2024-08-04 18:07:08 +0530

    Update Icons.jsx

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -3,9 +3,9 @@ import Icon from "./Icon"
 const Icons = () => {
   return (
     <div className="w-full h-[250px] flex justify-center items-center mt-[40px]" >
-        <Icon image="../../assets/fist.png" move="Rock" />
-        <Icon image ="../../assets/hand-paper.png" move="Paper"/>
-        <Icon image="../../assets/scissors (1).png" move="Scissors"/>
+        <Icon image="../assets/fist.png" move="Rock" />
+        <Icon image ="../assets/hand-paper.png" move="Paper"/>
+        <Icon image="../assets/scissors (1).png" move="Scissors"/>
     </div>
   )
 }
