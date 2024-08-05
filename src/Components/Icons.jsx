0e3c0350d4be3d804289701a3249aca878ae2523
shivanshp2004 <shivanshpathak9988@gmail.com>
2024-08-06 00:13:32 +0530

    deployed

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -2,7 +2,7 @@ import Icon from "./Icon"
 
 const Icons = () => {
   return (
-    <div className="w-full h-[250px] flex justify-center items-center mt-[40px] flex-wrap" >
+    <div className="w-full h-[250px] flex justify-center items-center mt-[40px] " >
         <Icon image="fist.png" move="Rock" />
         <Icon image ="hand-paper.png" move="Paper"/>
         <Icon image="scissors (1).png" move="Scissors"/>
