69ca8058a8ca9b73d785acf6741d9ce77df501f5
shivanshp2004 <shivanshpathak9988@gmail.com>
2024-08-05 23:36:35 +0530

    responsiveness

--- src/Components/Move.jsx
+++ src/Components/Move.jsx
@@ -2,8 +2,8 @@
 const Move = () => {
   return (
     <div className="w-full h-[100px] mt-[100px] flex justify-center items-center max-sm:mt-[40px]">
-      <div className="w-[460px] h-[70px] flex justify-center items-center bg-[#780000] rounded-2xl max-sm:w-[300px]">
-        <p className="text-[2.8rem] font-bold text-[#dad7cd] max-sm:text-[1.8rem]">Pick Your Move</p>
+      <div className="w-[460px] h-[70px] flex justify-center items-center bg-[#780000] rounded-2xl max-sm:w-[200px]">
+        <p className="text-[2.8rem] font-bold text-[#dad7cd] max-sm:text-[1.4rem]">Pick Your Move</p>
       </div>
     </div>
   )
